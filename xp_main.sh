dd61d58ef7b11c65e5b79e0a523e7f2e719d3f18
aethor <oow.autre@gmail.com>
2025-04-06 14:08:39 +0800

    -> go to 10 epochs max for early stopping

--- xp_main.sh
+++ xp_main.sh
@@ -8,9 +8,10 @@ python xp_main.py -F './runs' with\
        hg_training_kwargs.evaluation_strategy="epoch"\
        hg_training_kwargs.per_device_train_batch_size=8\
        hg_training_kwargs.per_device_eval_batch_size=8\
-       hg_training_kwargs.num_train_epochs=3\
+       hg_training_kwargs.num_train_epochs=10\
        hg_training_kwargs.save_total_limit=3\
        hg_training_kwargs.eval_accumulation_steps=32\
+       hg_training_kwargs.eval_strategy="epoch"\
        hg_training_kwargs.learning_rate=1e-5\
        use_weights=True\
        co_occurrences_dist=32
